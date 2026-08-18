ab6848d36c1691c60e6e009ebd0772fc48d77750
Alex <alexan6373@gmail.com>
2026-08-18 15:03:18 -0700

    Started Tic Tac Toe game.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -112,16 +112,86 @@ void TicTacToe::draw(sf::RenderWindow& window) {
     sf::Text title(font, "TIC TAC TOE", 80);
     sf::FloatRect bounds = title.getLocalBounds();
     title.setOrigin({ bounds.position.x + bounds.size.x / 2.f, bounds.position.y + bounds.size.y / 2.f });
-    title.setPosition({  window.getSize().x / 2.f, 50 });
+    title.setPosition({window.getSize().x / 2.f, 50});
+    
+    int move;
+    
+    const float boardSize = 570.f;
+    const float cellSize = 190.f;
+    
+    float startX = window.getSize().x / 2.f - boardSize / 2.f;
+    float startY = window.getSize().y / 2.f - boardSize / 2.f;
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
+        
+            if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+                if (mouseButtonPressed->button == sf::Mouse::Button::Left) {
+                    int mouseX = static_cast<float>(mouseButtonPressed->position.x);
+                    int mouseY = static_cast<float>(mouseButtonPressed->position.y);
+                    
+                    
+                    if (startX >= startX && startX < startX + boardSize &&
+                        startY >= startY && mouseY < startY + boardSize) {
+                        
+                        move = 3 * (int) ((mouseY - startY) / cellSize) + (int) ((mouseX - startX) / cellSize);
+                        
+                        if (move < 0 || move > 8 || board[move / 3][move % 3] != ' ') {
+                            std::cout << "Invalid move." << std::endl << std::endl;
+                            continue;
+                        } else {
+                            updateBoard(move);
+                            if (isWinner(playerToMove)) {
+                                std::cout << "The game is over. Player " << playerToMove << " won." << std::endl;
+                                break;
+                            } else if (isBoardFull()) {
+                                std::cout << "The game is a draw." << std::endl;
+                                break;
+                            }
+                            
+                            playerToMove = playerToMove == 'X' ? 'O' : 'X';
+                        }
+                    }
+                }
+            }
         }
 
         window.clear();
         window.draw(title);
+        
+        for (int i = 0; i < 4; i++) {
+            sf::RectangleShape line({5.f, boardSize});
+            line.setPosition({startX + i * cellSize, startY});
+            window.draw(line);
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            sf::RectangleShape line({boardSize, 5.f});
+            line.setPosition({startX, startY + i * cellSize});
+            window.draw(line);
+        }
+        
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == 'X') {
+                    sf::Text xTile(font, "X", 150);
+                    sf::FloatRect bounds = xTile.getLocalBounds();
+                    xTile.setOrigin({bounds.position.x + bounds.size.x / 2.f, bounds.position.y + bounds.size.y / 2.f});
+                    xTile.setPosition({startX + cellSize / 2 + row * cellSize, startY + cellSize / 2 + col * cellSize});
+                    window.draw(xTile);
+                    
+                } else if (board[row][col] == 'O') {
+                    sf::Text oTile(font, "O", 150);
+                    sf::FloatRect bounds = oTile.getLocalBounds();
+                    oTile.setOrigin({bounds.position.x + bounds.size.x / 2.f, bounds.position.y + bounds.size.y / 2.f});
+                    oTile.setPosition({startX + cellSize / 2 + row * cellSize, startY + cellSize / 2 + col * cellSize});
+                    window.draw(oTile);
+                }
+            }
+        }
+        
         window.display();
     }
 }
